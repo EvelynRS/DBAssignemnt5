7eb9cc4d1c6715be628ec6991300a30d734614f1
thelittleblob <77759810+thelittleblob@users.noreply.github.com>
2021-04-19 14:58:27 -0500

    Add files via upload

--- viewAllTeams.php
+++ viewAllTeams.php
@@ -18,11 +18,39 @@
 <p style="text-align:center">See all the teams below!</p>
 <br>
 <div class="content">
-PLACEHOLDER
+
+
+<?php
+$servername = "localhost";
+$username = "mjk006";
+$password = "aiPh2tiu";
+$dbname = "mjk006";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT TEAM_ID, TEAM_NAME, NICKNAME, RANK FROM team";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    echo "<table><tr><th>Team ID</th><th>Team Name</th><th>Team Nickname</th><th>Team Rank</th></tr>";
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<tr><td>" . $row["TEAM_ID"]. "</td><td>" . $row["TEAM_NAME"]. "</td><td> " . $row["NICKNAME"]. "</td><td> " . $row["RANK"]. "</td></tr>";
+    }
+    echo "</table>";
+} else {
+    echo "0 results";
+}
+
+$conn->close();
+?>
+
 </div>
 
 </body>
 </html>
-
-<?php
-?>
