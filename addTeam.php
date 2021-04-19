61afe596fffcaa4cd6f9c232a0e7a4d71443fd83
thelittleblob <77759810+thelittleblob@users.noreply.github.com>
2021-04-19 15:01:11 -0500

    Progress update - not currently functional

--- addTeam.php
+++ addTeam.php
@@ -27,10 +27,50 @@
         <input type="text" name="rank" id="trank" value="<?php echo $rank;?>"> <br>
         <input type="submit" value="Add my team!">
     </form>
+
+
+<?php
+if (isset($_POST['submit'])) 
+{
+    // replace ' ' with '\ ' in the strings so they are treated as single command line args
+    $tname = escapeshellarg($_POST[tname]);
+    $nname = escapeshellarg($_POST[nname]);
+    $trank = escapeshellarg($_POST[trank]);
+
+    $servername = "localhost";
+    $username = "mjk006";
+    $password = "aiPh2tiu";
+    $dbname = "mjk006";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+    
+    $sql2 = "SELECT COUNT(TEAM_ID) FROM team";
+    $result = $conn->query($sql2);
+    $result = $result + 1;
+    
+    //$data = mysql_fetch_assoc($result);
+    //$data = $data + 1;
+    
+    $sql = "INSERT INTO team (TEAM_ID, TEAM_NAME, NICKNAME, RANK)
+    VALUES ($result, $tname, $nname, $trank)";
+
+    if ($conn->query($sql) === TRUE) {
+      echo "New record created successfully";
+    } else {
+      echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+         
+}
+$conn->close(); 
+?>
+
 </div>
 
 </body>
 </html>
-
-<?php
-?>
