d9d1981fe71ffcd12f81dad5f96a61f671a5b27a
thelittleblob <77759810+thelittleblob@users.noreply.github.com>
2021-04-21 20:06:56 -0500

    Functional

--- addResult.php
+++ addResult.php
@@ -15,19 +15,56 @@
     <a href="viewDateResults.php">View results from a date</a>
 </div>
 
+<p style="text-align:center">See all the Game IDs below!</p>
+<br>
+<div class="content">
+
+
+<?php
+$servername = "localhost";
+$username = "mjk006";
+$password = "aiPh2tiu";
+$dbname = "mjk006";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT GAME_ID, RANK1, RANK2, LOCATION, DATE FROM game";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    echo "<table><tr><th>Game ID</th><th>Home Team Rank</th><th>Away Team Rank</th><th>Location</th><th>Date</th></tr>";
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<tr><td>" . $row["GAME_ID"]. "</td><td>" . $row["RANK1"]. "</td><td> " . $row["RANK2"]. "</td><td> " . $row["LOCATION"]. "</td><td>" . $row["DATE"]. "</td></tr> ";
+    }
+    echo "</table>";
+} else {
+    echo "0 results";
+}
+
+$conn->close();
+?>
+
+
+
+
+
+
 <p style="text-align:center">Here you can add a game's results to the database! To do so, just fill out the form below.</p>
 <br>
 <div class="content">
-<form class="forms">
-    <label for="ht">Home team:</label>
-    <input type="text" name="hometeam" id="ht" value="<?php echo $hometeam;?>"> <br>
-    <label for="at">Away team:</label>
-    <input type="text" name="awayteam" id="at" value="<?php echo $awayteam;?>"> <br>
-    <label for="hts">Home team score:</label>
-    <input type="text" name="homescore" id="hts" value="<?php echo $homescore;?>"> <br>
-    <label for="ats">Away team score:</label><br>
-    <input type="text" name="awayscore" id="ats" value="<?php echo $awayscore;?>">
-    <input type="submit" value="Add my results!">
+<form action="addResult.php" method="post" class="forms">
+    Game ID:<input type="text" name="game_id"><br>
+    Home Team ID:<input type="text" name="home_team"><br>
+    Away Team ID:<input type="text" name="away_team"><br>
+    Home Team Score:<input type="text" name="home_score"><br>
+    Away Team Score:<input type="text" name="away_score"><br>    
+    <input name="submit" type="submit" value="Add my results!">
 </form>
 </div>
 
@@ -35,4 +72,49 @@
 </html>
 
 <?php
+if (isset($_POST['submit'])) 
+{
+    //echo "made it here";
+    // replace ' ' with '\ ' in the strings so they are treated as single command line args
+    //variables are immediately setup to be in the proper format for sql insertion
+    $gameid = $_POST["game_id"];
+    $gameid = "'" . (string)$gameid . "'";
+    
+    $homename = $_POST["home_team"];
+    $homename = "'" . (string)$homename . "'";
+    
+    $awayname = $_POST["away_team"];
+    $awayname = "'" . (string)$awayname . "'";
+    
+    $homescore = $_POST["home_score"];
+    $homescore = "'" . (string)$homescore . "'";
+    
+    $awayscore = $_POST["away_score"];
+    $awayscore = "'" . (string)$awayscore . "'";
+
+    
+    $servername = "localhost";
+    $username = "mjk006";
+    $password = "aiPh2tiu";
+    $dbname = "mjk006";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+        
+    $sql = "INSERT INTO result (GAME_ID, TEAM_ONE_ID, TEAM_TWO_ID, SCORE_ONE, SCORE_TWO)
+    VALUES ($gameid, $homename, $awayname, $homescore, $awayscore)";
+
+    if ($conn->query($sql) === TRUE) {
+      echo "New record created successfully";
+    } else {
+      echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+         
+}
+$conn->close(); 
 ?>
